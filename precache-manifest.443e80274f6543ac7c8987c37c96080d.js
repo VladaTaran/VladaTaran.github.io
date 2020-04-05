7a081f5ce059e57bc03ecb5d8457872c3018b787
vlada <n.vladyslava@gmail.com>
2020-04-05 20:15:03 +0300

    Updates

---
+++ precache-manifest.443e80274f6543ac7c8987c37c96080d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf9ae4736f7af5555dba31ecc9365e6",
+    "url": "/guess-word-game/index.html"
+  },
+  {
+    "revision": "79f19595777e1d1df6b2",
+    "url": "/guess-word-game/static/css/2.52f67402.chunk.css"
+  },
+  {
+    "revision": "bad14e15c6790986d33b",
+    "url": "/guess-word-game/static/css/main.8eb71605.chunk.css"
+  },
+  {
+    "revision": "79f19595777e1d1df6b2",
+    "url": "/guess-word-game/static/js/2.b22db872.chunk.js"
+  },
+  {
+    "revision": "192201c36557064462a543b746126438",
+    "url": "/guess-word-game/static/js/2.b22db872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad14e15c6790986d33b",
+    "url": "/guess-word-game/static/js/main.145b134d.chunk.js"
+  },
+  {
+    "revision": "3463f8181ae8944f1def",
+    "url": "/guess-word-game/static/js/runtime-main.470dccd4.js"
+  },
+  {
+    "revision": "c56718d29292b9b87960ab02b035991a",
+    "url": "/guess-word-game/static/media/blue.c56718d2.png"
+  },
+  {
+    "revision": "5e3b899956697b44019f1d59e758256f",
+    "url": "/guess-word-game/static/media/dark-blue.5e3b8999.png"
+  },
+  {
+    "revision": "0bbb3d8f69f684d99f7f457f8d761468",
+    "url": "/guess-word-game/static/media/green.0bbb3d8f.png"
+  },
+  {
+    "revision": "41c6ea9f7e96933b45d5b022d4031f28",
+    "url": "/guess-word-game/static/media/grey.41c6ea9f.png"
+  },
+  {
+    "revision": "e8150a7915c5b1105e8bbf5adc90e6bc",
+    "url": "/guess-word-game/static/media/pink.e8150a79.png"
+  }
+]);
